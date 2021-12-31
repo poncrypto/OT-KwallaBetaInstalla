8db1b76583ad09cdaa2dc69852671b55c2d20a03
calvin <calr0x@gmail.com>
2021-12-31 16:12:22 -0600

    .

--- data/add-ssl.sh
+++ data/add-ssl.sh
@@ -5,6 +5,20 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
+echo -n "(SSL) Creating cert directory: "
+
+OUTPUT=$(rm -rf /root/certs && mkdir /root/certs >/dev/null 2>&1)
+
+if [[ $? -eq 1 ]]; then
+    echo -e "${RED}FAILED${NC}"
+    echo "There was an error creating the certs directory."
+    echo $OUTPUT
+    exit 1
+else
+    echo -e "${GREEN}SUCCESS${NC}"
+fi
+
+
 
 openssl genrsa > privkey.pem
 openssl req -new -x509 -key privkey.pem > fullchain.pem
